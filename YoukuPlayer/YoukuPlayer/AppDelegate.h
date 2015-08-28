711db6171a26d44b153004752ea96af028f27887
LiuXuanyi <xuanyicode@icloud.com>
2015-08-28 16:55:10 +0800

    创建项目

---
+++ YoukuPlayer/YoukuPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YoukuPlayer
+//
+//  Created by Echo on 8/28/15.
+//  Copyright (c) 2015 Echo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YoukuPlayer/YoukuPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YoukuPlayer
+//
+//  Created by Echo on 8/28/15.
+//  Copyright (c) 2015 Echo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
